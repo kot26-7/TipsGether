98839c99dbbc270628a74fc88e8d8dc603d8dc68
kot26-7 <taro726896oka@gmail.com>
2020-10-12 13:32:11 +0900

    set users validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  before_save { self.email = email.downcase }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  VALID_USERNAME_REGEX = /\A[a-zA-Z0-9_]+\z/.freeze
+  validates :email, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :username, presence: true, length: { maximum: 50 },
+                       format: { with: VALID_USERNAME_REGEX }
+  validates :introduce, length: { maximum: 500}
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :user do
-    
+    id { 1 }
+    username { 'test' }
+    email { 'TESTtest@example.com' }
+    password { 'password' }
+    introduce { 'Hi, im sample.' }
   end
 end

325101a245827dda631f8d3cc6e1a16bf2aeb705
kot26-7 <taro726896oka@gmail.com>
2020-10-13 13:49:29 +0900

    add post data in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,42 @@
-User.create(id: 1, username: 'testuser', email: 'sampleuser@example.com', password: 'password')
-User.create(id: 2, username: 'otheruser', email: 'sampleuser1@example.com', password: 'password')
+User.create(
+  [
+    {
+      id: 1,
+      username: 'testuser',
+      email: 'sampleuser@example.com',
+      password: 'password'
+    },
+    {
+      id: 2,
+      username: 'otheruser',
+      email: 'sampleuser1@example.com',
+      password: 'password'
+    }
+  ]
+)
+Post.create(
+  [
+    {
+      user_id: 1,
+      title: 'sample title 1',
+      content: 'this is sample text okay'
+    },
+    {
+      user_id: 1,
+      title: 'sample title 2',
+      content: 'this is sample text okay',
+      published: true
+    },
+    {
+      user_id: 2,
+      title: 'sample title 3',
+      content: 'this is sample text okay'
+    },
+    {
+      user_id: 2,
+      title: 'sample title 4',
+      content: 'this is sample text okay',
+      published: true
+    }
+  ]
+)

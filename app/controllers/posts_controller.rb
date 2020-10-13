873bd5f6fde345498f29768998281e5128b38770
kot26-7 <taro726896oka@gmail.com>
2020-10-13 11:29:27 +0900

    create posts cons actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,54 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :correct_post_user, only: [:edit, :update, :destroy]
+
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to post_path(@post), notice: '投稿しました。'
+    else
+      render :new
+    end
   end
 
   def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to post_path(@post), notice: '投稿が更新されました。'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to user_path(post.user), notice: '投稿が削除されました。'
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :published)
+  end
+
+  def correct_post_user
+    @post = Post.find(params[:id])
+    unless @post.user_id == current_user.id
+      redirect_to user_path(current_user), alert: Settings.invalid_access_msg
+    end
   end
 end

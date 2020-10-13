314708a626e95505724fee45d2c095de2d6a5e5b
kot26-7 <taro726896oka@gmail.com>
2020-10-13 12:34:54 +0900

    add header links and set posts index page layout

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :correct_post_user, only: [:edit, :update, :destroy]
 
   def index
-    @posts = Post.all
+    @posts = Post.where(published: true).includes(:user)
   end
 
   def show

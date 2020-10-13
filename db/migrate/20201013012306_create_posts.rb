2833305f8cdacf6f47d149e3260b1a7c7cd46c5b
kot26-7 <taro726896oka@gmail.com>
2020-10-13 10:27:44 +0900

    create post model

---
+++ db/migrate/20201013012306_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.string :title, null: false, default: ""
+      t.text :content, null: false, default: ""
+      t.boolean :published, null: false, default: false
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end

5d3bccc77509a8485efb2aea7776a21a23fdd13f
kot26-7 <taro726896oka@gmail.com>
2020-10-15 20:09:41 +0900

    create favorite controller and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_posts, through: :favorites, source: :post
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   before_save { self.email = email.downcase }

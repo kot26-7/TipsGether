0593efd28951b1af0ef71b6efa29c0dcdee3b21e
kot26-7 <taro726896oka@gmail.com>
2020-10-27 00:13:01 +0900

    create comment create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.user_id = current_user.id
+    if @comment.save
+      redirect_back(fallback_location: root_path), notice: 'コメントしました。'
+    else
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,7 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     @favorite = Favorite.new
+    @comment = Comment.new
   end
 
   def new

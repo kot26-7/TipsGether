0dc863fbc13ef6b8236c2411adac95384084d208
kot26-7 <taro726896oka@gmail.com>
2020-10-28 20:41:57 +0900

    add image field

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,7 +38,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :email, :introduce)
+    params.require(:user).permit(:username, :email, :introduce, :image)
   end
 
   def correct_user
